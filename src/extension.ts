02a76338558d5ebbca3572873afcdda498f31fc5
Scott Tolinski <scott.tolinski@gmail.com>
2024-08-26 09:23:48 -0600

    adds new show

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-import * as _ from 'lodash';
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 let currentEditor: vscode.TextEditor;
 
@@ -11,82 +10,15 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.onDidChangeActiveTextEditor((editor) => (currentEditor = editor));
 
   context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.nameValue',
-      (editor, edit) => handle(Wrap.Down, true, 'nameValue')
+    vscode.commands.registerTextEditorCommand("svelte.wrap.inspect", (editor, edit) =>
+      handle(Wrap.Down, true, "inspect")
     )
   );
-
-
   context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.name',
-      (editor, edit) => handle(Wrap.Down, true, 'name')
+    vscode.commands.registerTextEditorCommand("svelte.wrap.effect", (editor, edit) =>
+      handle(Wrap.Down, true, "effect")
     )
   );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.arguments',
-      (editor, edit) => handle(Wrap.Down, true, 'arguments')
-    )
-  );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.get',
-      (editor, edit) => handle(Wrap.Down, true, 'get')
-    )
-  );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.return',
-      (editor, edit) => handle(Wrap.Down, true, 'return')
-    )
-  );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.json',
-      (editor, edit) => handle(Wrap.Down, true, 'json')
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.block',
-      (editor, edit) => handle(Wrap.Down, true, 'block')
-    )
-  );
-  // context.subscriptions.push(
-  //   vscode.commands.registerTextEditorCommand('console.log.wrap.labelValue',
-  //     (editor, edit) => handle(Wrap.Down, true, 'labelValue')
-  //   )
-  // );
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.map',
-      (editor, edit) => handle(Wrap.Down, true, 'map')
-    )
-  );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.for',
-      (editor, edit) => handle(Wrap.Down, true, 'for')
-    )
-  );
-
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.forEach',
-      (editor, edit) => handle(Wrap.Down, true, 'forEach')
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerTextEditorCommand('console.log.wrap.expect',
-      (editor, edit) => handle(Wrap.Down, true, 'expect')
-    )
-  );
-
 }
 
 function handle(target: Wrap, prefix?: boolean, type?: string) {
@@ -100,7 +32,7 @@ function handle(target: Wrap, prefix?: boolean, type?: string) {
         : new vscode.Range(sel.start, sel.end);
 
     if (ran == undefined) {
-      reject('NO_WORD');
+      reject("NO_WORD");
     } else {
       let doc = currentEditor.document;
       let lineNumber = ran.start.line;
@@ -108,9 +40,9 @@ function handle(target: Wrap, prefix?: boolean, type?: string) {
 
       let idx = doc.lineAt(lineNumber).firstNonWhitespaceCharacterIndex;
       let ind = doc.lineAt(lineNumber).text.substring(0, idx);
-      const funcName = getSetting('functionName');
+      const funcName = getSetting("functionName");
       let wrapData: any = {
-        txt: getSetting('functionName'),
+        txt: getSetting("functionName"),
         item: item,
         doc: doc,
         ran: ran,
@@ -120,51 +52,54 @@ function handle(target: Wrap, prefix?: boolean, type?: string) {
         sel: sel,
         lastLine: doc.lineCount - 1 == lineNumber,
       };
-      const semicolon = ';';
-      if (type === 'nameValue') {
-        if (wrapData.item.includes(',')) {
-          const items = wrapData.item.split(',').map((item) => item?.split(':')?.[0]?.split('?')?.[0].trim()).filter(i => i)
-          wrapData.txt = '';
+
+      if (type === "nameValue") {
+        if (wrapData.item.includes(",")) {
+          const items = wrapData.item
+            .split(",")
+            .map((item) => item?.split(":")?.[0]?.split("?")?.[0].trim())
+            .filter((i) => i);
+          wrapData.txt = "";
           for (const item of items) {
-            wrapData.txt += (funcName + "('".concat(item, "', ", item, ')', semicolon) + "\n" + ind);
+            wrapData.txt += funcName + "('".concat(item, "', ", item, ")") + "\n" + ind;
           }
-        } else {
-          wrapData.txt = funcName + "('".concat(wrapData.item, "', ", wrapData.item, ')', semicolon);
         }
-      } else if (type === 'arguments') {
-        wrapData.txt = funcName + "('".concat(wrapData.item, "', ", 'arguments', ')', semicolon);
-      } else if (type === 'get') {
-        wrapData.txt = "const aaa = get(".concat(wrapData.item, ", '", 'aaa', "', '')", semicolon);
-      } else if (type === 'return') {
-        wrapData.txt = "return ".concat(wrapData.item, semicolon);
-      } else if (type === 'json') {
-        wrapData.txt = funcName + "('".concat(wrapData.item, "', JSON.stringify(", wrapData.item, ", null, 2))", semicolon);
-      } else if (type === 'block') {
-        wrapData.txt = "console.log('".concat("\\n\\n%c--------- ", wrapData.item, " ---------', 'background:yellow; color:blue; font-weight:600;')", semicolon);
+      } else if (type === "inspect") {
+        wrapData.txt = funcName + "('".concat(wrapData.item, "', ", "arguments", ")");
+
+        // } else if (type === 'get') {
+        //   wrapData.txt = "const aaa = get(".concat(wrapData.item, ", '", 'aaa', "', '')", semicolon);
+      } else if (type === "inspect") {
+        wrapData.txt = `$inspect(${wrapData.item});`;
       }
+      // } else if (type === 'json') {
+      //   wrapData.txt = funcName + "('".concat(wrapData.item, "', JSON.stringify(", wrapData.item, ", null, 2))", semicolon);
+      // } else if (type === 'block') {
+      //   wrapData.txt = "console.log('".concat("\\n\\n%c--------- ", wrapData.item, " ---------', 'background:yellow; color:blue; font-weight:600;')", semicolon);
+      // }
       // else if (type === 'labelValue') {
       //   wrapData.txt = "console.info('".concat("%c ", wrapData.item, "', 'color:green; font-weight:600;', ", wrapData.item, ")", semicolon);
       // }
-      else if (type === 'expect') {
-        wrapData.txt = "expect(".concat(wrapData.item, ").toBeDefined();");
-      } else if (type === 'map') {
-        wrapData.txt = `${wrapData.item}.map((item) => {
-  return {
-    ...item,
-  };
-})`
-      } else if (type === 'for') {
-        wrapData.txt = `for (let index = 0; index < ${wrapData.item}.length; index++) {
-          const item = ${wrapData.item}[index];
-          
-        }`
-      } else if (type === 'forEach') {
-        wrapData.txt = `${wrapData.item}.forEach(item => {
-          
-        });`
-      } else {
-        wrapData.txt = funcName + "('".concat(wrapData.item, "')", semicolon);
-      }
+      //       else if (type === 'expect') {
+      //         wrapData.txt = "expect(".concat(wrapData.item, ").toBeDefined();");
+      //       } else if (type === 'map') {
+      //         wrapData.txt = `${wrapData.item}.map((item) => {
+      //   return {
+      //     ...item,
+      //   };
+      // })`
+      //       } else if (type === 'for') {
+      //         wrapData.txt = `for (let index = 0; index < ${wrapData.item}.length; index++) {
+      //           const item = ${wrapData.item}[index];
+
+      //         }`
+      //       } else if (type === 'forEach') {
+      //         wrapData.txt = `${wrapData.item}.forEach(item => {
+
+      //         });`
+      //       } else {
+      //         wrapData.txt = funcName + "('".concat(wrapData.item, "')", semicolon);
+      //       }
       resolve(wrapData);
     }
   })
@@ -176,30 +111,25 @@ function handle(target: Wrap, prefix?: boolean, type?: string) {
         nxtLine = wrap.doc.lineAt(wrap.line + 1);
         nxtLineInd = nxtLine.text.substring(0, nxtLine.firstNonWhitespaceCharacterIndex);
       } else {
-        nxtLineInd = '';
+        nxtLineInd = "";
       }
       currentEditor
         .edit((e) => {
           e.insert(
-            new vscode.Position(
-              wrap.line,
-              wrap.doc.lineAt(wrap.line).range.end.character
-            ),
-            '\n'.concat(nxtLineInd > wrap.ind ? nxtLineInd : wrap.ind, wrap.txt)
+            new vscode.Position(wrap.line, wrap.doc.lineAt(wrap.line).range.end.character),
+            "\n" + ((nxtLineInd > wrap.ind ? nxtLineInd : wrap.ind) + wrap.txt)
           );
         })
         .then(() => {
           currentEditor.selection = wrap.sel;
         });
     })
-    .catch((message) => {
-    });
+    .catch((message) => {});
 }
 
 function getSetting(setting: string) {
-  return vscode.workspace.getConfiguration('wrap-console-log-simple')[setting];
+  return vscode.workspace.getConfiguration("wrap-svelte")[setting];
 }
-
 
 interface WrapData {
   txt: string;
